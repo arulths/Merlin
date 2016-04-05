d458c5b1a0a785c7cb5a2b594c41d7442810f022
Stephan <arulths@mcmaster.ca>
2016-04-05 17:10:29 -0400

    Upload Merlin GUI

---
+++ Merlin_GUI/app/src/main/java/com/mcmaster/sfwr/merlin_gui/HomePage.java
@@ -0,0 +1,26 @@
+package com.mcmaster.sfwr.merlin_gui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatAutoCompleteTextView;
+import android.view.Menu;
+
+/**
+ * Created by Stephan on 2016-04-05.
+ */
+public class HomePage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage_gui);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate actionbar
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+}
